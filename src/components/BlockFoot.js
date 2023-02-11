a533f650614c367c259b4c4d94dcf58ee1cee887
colins1 <maxrozhkov1@gmail.com>
2023-02-12 00:32:14 +0300

    add

--- src/components/BlockFoot.js
+++ src/components/BlockFoot.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {footBlocks} from '../data/footBlocks'
-import nanoid from 'nanoid';
 
 
 /**
@@ -10,7 +9,7 @@ import nanoid from 'nanoid';
 
 export default function BlockFoot() {
     return (
-        <div className='block'>{footBlocks.map(el => <div key={nanoid()} className='footBlocks'>
+        <div className='block'>{footBlocks.map(el => <div key={el.title} className='footBlocks'>
                 <h2>{el.title}</h2>
                 <p>{el.text}</p>
             </div>)}
